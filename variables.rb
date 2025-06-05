c288f3d5fb69aca4cbcada277f80684562208015
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-06-05 21:30:30 +0000

    Codespace_integration

---
+++ variables.rb
@@ -0,0 +1,33 @@
+# Declaring a variable
+
+age = 18
+
+age = 18 + 5
+
+# Reassigning variable
+age = 33
+
+
+# Trying division
+temperature = 10
+
+temperature /= 2
+
+p temperature
+
+john = "ryan"
+
+# What does ! mean? No?
+p john.upcase
+
+p john.upcase!
+
+
+# loop
+
+i = 0
+loop do
+    puts "i is #{i}"
+    i += 1
+    break if == 10
+end
