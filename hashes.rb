c39d27ce1fd171726782654defff8d935401b06a
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-06-06 23:59:44 +0200

    Create hashes.rb

---
+++ hashes.rb
@@ -0,0 +1,8 @@
+#Using Odin project templates to understand the structure
+
+first_hash = {
+  "a random word" => "ahoy",
+  "Dorothy's math test score" => 94,
+  "an array" => [1, 2, 3]
+  "an empty hash within a hash" => {}
+} 
